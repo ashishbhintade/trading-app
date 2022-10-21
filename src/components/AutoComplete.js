5dab1dc89a656a5a78d43f67bc2dfed1f80aff8f
Ashish Bhintade <68814285+ashishbhintade@users.noreply.github.com>
2022-10-21 19:18:10 +0530

    Autocomplete search

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -1,3 +1,27 @@
+import { useState } from "react";
+
 export const AutoComplete = () => {
-  return <div>AutoComplete</div>;
+  const [search, setSearch] = useState("");
+  return (
+    <div className="w-50 p-5 rounded mx-auto">
+      <div className="form-floating dropdown">
+        <input
+          style={{ backgroundColor: "rgba(145, 158, 171, 0.04)" }}
+          id="search"
+          type="text"
+          className="form-control"
+          placeholder="Search"
+          autoComplete="off"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        ></input>
+        <label htmlFor="search">Search</label>
+        <ul className="dropdown-menu show">
+          <li>stock1</li>
+          <li>stock2</li>
+          <li>stock3</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
