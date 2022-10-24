0a3f0010a2e8304049f65348f6796b44b5eefbb3
Ashish Bhintade <68814285+ashishbhintade@users.noreply.github.com>
2022-10-24 11:00:00 +0530

    Fetching historical data

--- src/components/StockList.js
+++ src/components/StockList.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { BsFillCaretDownFill, BsCaretUpFill } from "react-icons/bs";
 import finnHub from "../apis/finnHub";
 import { WatchListContext } from "../context/watchListContext";
@@ -6,6 +7,7 @@ import { WatchListContext } from "../context/watchListContext";
 export const StockList = () => {
   const [stock, setStock] = useState([]);
   const { watchList } = useContext(WatchListContext);
+  const navigate = useNavigate();
 
   const changeColor = (change) => {
     return change > 0 ? "success" : "danger";
@@ -43,6 +45,11 @@ export const StockList = () => {
     fetchData();
     return () => (isMounted = false);
   }, [watchList]);
+
+  const handleStockSelect = (symbol) => {
+    navigate(`detail/${symbol}`);
+  };
+
   return (
     <div>
       <table className="table hover mt-5">
@@ -61,7 +68,12 @@ export const StockList = () => {
         <tbody>
           {stock.map((stockData) => {
             return (
-              <tr className="table-row" key={stockData.symbol}>
+              <tr
+                style={{ cursor: "pointer" }}
+                onClick={() => handleStockSelect(stockData.symbol)}
+                className="table-row"
+                key={stockData.symbol}
+              >
                 <th scope="row">{stockData.symbol}</th>
                 <td>{stockData.data.c}</td>
                 <td className={`text-${changeColor(stockData.data.d)}`}>
